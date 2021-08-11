703ea7897a207481afa175ced1e1dfc938b3c30a
Allam9573 <aallamdaniel@gmail.com>
2021-08-11 13:56:05 -0600

    cambios a errores de 404 devolviendo al home

--- app.py
+++ app.py
@@ -31,10 +31,35 @@ def register():
     return render_template('response.html', nombre=nombre)
 
 
+@app.route('/mis_contactos')
+def getContactos():
+    cur = mysql.cursor(pymysql.cursors.DictCursor)
+    cur.execute('SELECT * FROM contactos')
+    data = cur.fetchall()
+    return render_template('miscontactos.html', contacts=data, title='Mis Contactos')
+
+
+@app.route('/delete_contact/<id_user>')
+def delete(id_user):
+    id_user = int(id_user)
+    cur = mysql.cursor(pymysql.cursors.DictCursor)
+    query = f'DELETE FROM contactos WHERE id_contacto = {id_user}'
+    cur.execute(query)
+    mysql.commit()
+    cur.close()
+    return make_response(redirect('/mis_contactos'))
+
+
 @app.errorhandler(404)
 def error(error):
-    return render_template('error404.html')
+    return make_response(redirect('/'))
+
+
+@app.errorhandler(400)
+def server(error):
+    return make_response(redirect('/error404.html'))
 
 
 if __name__ == '__main__':
     app.run()
+    
